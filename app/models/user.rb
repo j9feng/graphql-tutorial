92c55fa3c03c32305263d784ac3ac72449f043fe
Jianjun Feng <jianjun.feng@gmail.com>
2022-02-24 23:25:40 -0600

    add graphql mutation to create a new User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_secure_passwod
+    has_secure_password
 
-    validates :name, presence :true    
-    validates :email, presence :true, uniqueness: true   
+    validates :name, presence: true    
+    validates :email, presence: true, uniqueness: true   
 end
