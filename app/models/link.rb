0f883489e90599bb0ec553ffdfccd6a9813830c2
Jianjun Feng <jianjun.feng@gmail.com>
2022-02-25 16:58:48 -0600

    add Vote and add mutation for create votes

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,7 @@
 class Link < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: true
+
+    validates :url, presence: true, length: {minimum:5, maximum:140}
+    validates :description, presence: true, length: {minimum:5, maximum:280}
+    has_many :votes
 end
